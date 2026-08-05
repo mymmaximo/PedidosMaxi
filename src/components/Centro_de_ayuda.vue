523736c12a4018fabb4232f6eec974d2cc187638
Maxi <maxgiesenow@gmail.com>
2026-08-05 10:46:31 -0300

    Cambios Dia 05/08/2026

--- src/components/Centro_de_ayuda.vue
+++ src/components/Centro_de_ayuda.vue
@@ -2,7 +2,7 @@
     <div class="cuerpo">
         <div class="pagina">
             <div class="flex w-full flex-col sm:flex-row">
-                <div>
+                <div class="start">
                     <div class="mb-2 lg:mb-5">
                         <h1 class="text-center">
                         Centro de Ayuda
