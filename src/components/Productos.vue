b25550f38b540a724a522558ba3a7c70aaf49441
Maxi <maxgiesenow@gmail.com>
2026-08-10 09:01:41 -0300

    Cambios Dia 06/08/2026

--- src/components/Productos.vue
+++ src/components/Productos.vue
@@ -984,6 +984,14 @@
                 }
             })
             const categ = await respuesta.json()
+            const OrdenDeseado = ["Accesorios", "Hardware", "Periféricos", "Audio", "Hogar y Deco.", "Indumentaria", "SimRacing", "Varios..."]
+            categ.sort((a, b) => {
+                let indexA = OrdenDeseado.indexOf(a.categoria)
+                let indexB = OrdenDeseado.indexOf(b.categoria)
+                if (indexA === -1) indexA = 999 
+                if (indexB === -1) indexB = 999 
+                return indexA - indexB
+            })
             ListaCategoria.value = categ
             CargarCarrito()
             clearTimeout(temporizador)
