a9b5e4ff73296c8a9295a24906a900ee59a6d6e9
Maxi <maxgiesenow@gmail.com>
2026-08-14 12:42:57 -0300

    Cambios Dia 12/08/2026

--- src/App.vue
+++ src/App.vue
@@ -369,13 +369,13 @@
                         </div>
                     </transition>
                 </div>
-                <div class="flex-col w-full">
+                <div class="flex-col w-full flex-1">
                     <router-view>
                     </router-view>
                 </div>
                 <footer class="relative 
                 bg-green-900 text-green-100 
-                py-10 mt-auto w-full z-40 "
+                py-10 w-full z-40"
                 >
                     <div class="max-w-7xl mx-auto px-5 grid grid-cols-1 md:grid-cols-4 gap-8">
                         <div class="flex flex-col gap-3">
