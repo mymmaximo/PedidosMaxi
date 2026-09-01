bca3cf8ff122355a07cbc44ec1bfb6bf49945884
Maxi <maxgiesenow@gmail.com>
2026-09-01 11:35:51 -0300

    Cambios Dia 01/09/2026 Configuración lista para Vercel

--- src/components/Mis_pedidos.vue
+++ src/components/Mis_pedidos.vue
@@ -3,13 +3,15 @@
         <div class="pagina">
             <div class="flex w-full flex-col sm:flex-row">
                 <!-- Filtros -->
-                <transition name="fade">
-                    <div v-if="(MostrarFiltro || MostrarNuevo)" 
-                    @click="MostrarFiltro = false; MostrarNuevo = false"
-                    class="fondo !z-30 sm:hidden cursor-pointer"
-                    >
-                    </div>
-                </transition>
+                <Teleport to="body">
+                    <transition name="fade">
+                        <div v-if="(MostrarFiltro || MostrarNuevo)" 
+                        @click="MostrarFiltro = false; MostrarNuevo = false"
+                        class="fondo !z-[35] sm:hidden cursor-pointer"
+                        >
+                        </div>
+                    </transition>
+                </Teleport>
                 <div :class="[
                     'bar', 
                     (MostrarFiltro || MostrarNuevo) ? 'translate-x-0 sm:w-72 lg:w-80' : '-translate-x-full sm:w-fit sm:translate-x-0'
