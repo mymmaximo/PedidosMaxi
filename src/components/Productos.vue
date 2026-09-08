d19a1fdb59ac5628745056d1997bf07af1283f95
Maxi <maxgiesenow@gmail.com>
2026-09-08 13:05:10 -0300

    Cambios Dia 08/09/2026

--- src/components/Productos.vue
+++ src/components/Productos.vue
@@ -701,7 +701,7 @@
                         >
                         ᯤ Abrir Filtros
                         </button>
-                            <div class="flex flex-row items-stretch w-full gap-3 mb-5">
+                            <div class="flex flex-row items-stretch w-full gap-3 mb-5">-
                                 <input @input="BusquedaProducto"
                                 type="text" 
                                 v-model="Busqueda" 
