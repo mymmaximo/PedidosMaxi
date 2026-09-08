47c42c0ed67fc0e4f7db580c31b0eee62fe9bf5e
Maxi <maxgiesenow@gmail.com>
2026-09-08 12:58:36 -0300

    Cambios Dia 08/09/2026

--- src/components/Productos.vue
+++ src/components/Productos.vue
@@ -717,10 +717,10 @@
                                 transition-all shadow-sm active:scale-95"
                                 >
                                     <span v-if="!VistaLista" class="text-xl">
-                                    🪟
+                                    ⊞
                                     </span>
                                     <span v-else class="text-xl">
-                                    📄
+                                    ☰
                                     </span>
                                 </button>
                             </div>
