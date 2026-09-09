aea4b7ef10477f64bb1619e67e6f1c10158952a8
Maxi <maxgiesenow@gmail.com>
2026-09-09 10:12:38 -0300

    Cambios Dia 09/09/2026

--- src/components/Usuarios.vue
+++ src/components/Usuarios.vue
@@ -96,8 +96,8 @@
                 :class="[
                 'bar', 
                 (MostrarFiltro || MostrarNuevo) 
-                    ? 'translate-x-0 opacity-100 visible sm:w-72 lg:w-80' 
-                    : '-translate-x-full opacity-0 invisible sm:opacity-100 sm:visible sm:w-fit sm:translate-x-0'
+                ? 'translate-x-0 opacity-100 visible sm:w-72 lg:w-80' 
+                : '-translate-x-full opacity-0 invisible sm:opacity-100 sm:visible sm:w-fit sm:translate-x-0'
                 ]">
                     <div class="hidden sm:block">
                         <h1 @click="MostrarFiltro = !MostrarFiltro ; MostrarNuevo = false"
