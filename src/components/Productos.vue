a0c9ccf497ffe4a9b81d28f4e0cd991adf707993
Maxi <maxgiesenow@gmail.com>
2026-09-15 11:11:40 -0300

    Cambios Dia 15/09/2026

--- src/components/Productos.vue
+++ src/components/Productos.vue
@@ -567,7 +567,7 @@
                                     >
                                     <div class="w-11 h-6 bg-gray-300 rounded-full peer 
                                     peer-checked:after:translate-x-full peer-checked:after:border-white 
-                                    after:content-[''] after:absolute after:top-0.5 after:left-[6px] 
+                                    after:content-[''] after:absolute after:left-[6px] 
                                     after:bg-white after:border-gray-300 after:border after:rounded-full 
                                     after:h-5 after:w-5 after:transition-all peer-checked:bg-green-500 shadow-inner">
                                     </div>
